7c929a66624dc5eb960cd9459ebe875d89e47703
Rafael López <rlm96@hotmail.com>
2022-07-30 01:19:35 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var cursor = true;
+var speed = 220;
+
+setInterval(() => {
+   if(cursor) {
+     document.getElementById('cursor').style.opacity = 0;
+     cursor = false;
+   }else {
+     document.getElementById('cursor').style.opacity = 1;
+     cursor = true;
+   }
+}, speed);
